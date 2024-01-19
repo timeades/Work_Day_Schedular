9b8e6868f9ef0dd6c5014881a7d4aa9962133b94
Tim Eades <tim.eades@mac.com>
2024-01-19 18:58:17 +0000

    added todays date to head of shedular

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // Display the current date and time in <#currentDay> <p> tag using day.js
 var rightNow = dayjs().format('dddd, MMMM D, YYYY h:mm:ss a');
-    $('#currentDay').text(rightNow);
+$('#currentDay').text(rightNow);
 // Present time blocks for standard business hours (9am - 5pm)
 
 // Color code time blocks to indicate whether it is in the past, present, or future
